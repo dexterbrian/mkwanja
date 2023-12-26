2f7dacdba8450cceb2c130bfa861e519e235aeda
Brian Waweru <brianwaweru627@gmail.com>
2023-12-26 18:36:48 +0300

    feat: created home page at /home

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import defaultProfileImage from '../../lib/images/3d-assets/3d-profile-default/3d-profile-default.png';
+    import './home.css';
+</script>
+
+<div>
+    <img id="profile" alt="default-profile" src={defaultProfileImage}>
+    <h4>Dexter</h4>
+    <p>Saved KES 189K / KES 1M</p>
+</div>
