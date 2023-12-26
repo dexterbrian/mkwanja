0c5ed2cf30221d447a902f59279d181eabf77e95
Brian Waweru <brianwaweru627@gmail.com>
2023-12-26 21:32:41 +0300

    style: added primary & secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
     mode: 'jit',
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-    extend: {},
+    extend: {
+        colors: {
+            primary: '#4b4efc',
+            secondary: '#9EAFFF'
+        }
+    },
     },
     plugins: [require("daisyui")],
 }
