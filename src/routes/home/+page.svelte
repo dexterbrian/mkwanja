855b8181354311630f58197a1b12bf26ae41444a
Brian Waweru <brianwaweru627@gmail.com>
2023-12-27 17:41:01 +0300

    feat: added Available Balance card & My Budget section

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -6,6 +6,49 @@
 <div id="main-container">
     <div id="ui">
         <ProfileContainer />
-        <div id="month"><strong>Dec 2023</strong></div>
+        <div id="month" class="font-bold">Dec 2023</div>
+
+        <!-- Available Balance Card -->
+        <div class="card bg-primary text-primary-content">
+            <div class="card-body">
+                <div class="flex flex-row justify-between">
+                    <h4 class="text-white font-bold text-sm">Available Balance</h4>
+                    <div id="edit-profile-button">
+                        <svg id="edit-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#4b4efc" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
+                    </div>
+                </div>
+                <h1 class="text-white font-bold text-3xl">KES 44,840</h1>
+            </div>
+        </div>
+
+        <!-- My Budget Section -->
+        <div id="my-budget" class="mt-6 text-black">
+            <div class="section-title">My Budget<span class="text-black text-xs ml-6">KES 200 left to budget</span></div>
+            <div id="budget-container" class="flex flex-row">
+                <div id="add-budget-button" class="flex flex-col justify-center">
+                    <svg id="add-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#4b4efc" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                </div>
+                <div class="carousel">
+                    <div class="carousel-item">
+                      Shopping
+                    </div> 
+                    <div class="carousel-item">
+                      Rent
+                    </div> 
+                    <div class="carousel-item">
+                      Wifi
+                    </div> 
+                    <div class="carousel-item">
+                      Transport
+                    </div> 
+                    <div class="carousel-item">
+                      Food
+                    </div> 
+                    <div class="carousel-item">
+                      Fun
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
