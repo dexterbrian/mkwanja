8deaa9485196de6ed0c0098d10080875adfe4e3a
Brian Waweru <brianwaweru627@gmail.com>
2024-01-03 14:58:03 +0300

    refactor: made BudgetCard component dynamic

--- src/lib/components/BudgetCard/BudgetCard.svelte
+++ src/lib/components/BudgetCard/BudgetCard.svelte
@@ -1,7 +1,9 @@
-<script></script>
+<script lang="ts">
+    export let name: string, amount: any;
+</script>
 
-<div class="my-budget-card">
-    <h4>Shopping</h4>
-    <strong>KES 4,000</strong>
-    <h4>12%</h4>
+<div class="my-budget-card border border-secondary rounded-xl p-4">
+    <h4>{name}</h4>
+    <strong>{amount}</strong>
+    <h4>0%</h4>
 </div>
